034ef98e2f000e563ab573436d652379d3048dbc
ygzdvr <hd0216@princeton.edu>
2023-10-27 16:55:42 -0400

    added the gig details

--- database.py
+++ database.py
@@ -309,6 +309,7 @@ def owns_gig(netid, gigID):
 def _test():
     check_and_add_user('ta0639')
     _close_all_connections()
+    print(get_gig_details(12).get_category())
     return
 
 if __name__ == '__main__':
