f3d8778588bff4edb610524146579f47d7c80de0
taylanoaydin <ta0639@princeton.edu>
2023-11-08 22:58:28 -0500

    lastactive logic added

--- database.py
+++ database.py
@@ -344,6 +344,22 @@ def set_visibility(netid, visible):
         return False
     finally:
         _put_connection(connection)
+
+def update_activity(netid):
+    connection = _get_connection()
+    try:
+        with connection.cursor() as cursor:
+            cursor.execute('BEGIN')
+            query = "UPDATE users SET last_active = %s"
+            query += " WHERE netid = %s"
+
+            cursor.execute(query, [datetime.now().date(), netid])
+            cursor.execute('COMMIT')
+            return True
+    except Exception as ex:
+        return False
+    finally:
+        _put_connection(connection)
 #-----------------------------------------------------------------------
 
 # BOOLEAN RETURN FUNCTIONS
